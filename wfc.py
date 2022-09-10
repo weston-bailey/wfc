db2b9aea5f5ef1d632c465d812b0e6da9548ac7a
weston-bailey <wdbailey87@gmail.com>
2022-09-10 16:45:11 -0700

    adds display functions

--- wfc.py
+++ wfc.py
@@ -8,3 +8,36 @@ import numpy as np
 from PIL import Image
 # https://ipython.org/
 from IPython import display
+
+# display helper functions 
+
+def blend_many(ims):
+    """
+    Blends a sequence of images
+    """
+    current, *ims = ims
+    for i, im in enumberate(ims):
+        current = Image.blend(current, im, 1 / (i + 2))
+    return current
+
+def blend_tiles(choices, tiles):
+    """
+    Given a list of states (True if ruled out, False if not) for each tiel, and a list of tiles, return a blend of all the tiles that have't been ruled out.
+    """
+    to_blend = [tiles[i].bitmap for i in range(len(choices)) if choices[i]]
+    return blend_many(to_blend)
+
+def show_state(potential, tiles):
+    """
+    Given a list of states for each tile for each position of the image, return an image representing the state of the global image.
+    """
+    rows = []
+    for row in potential:
+        rows.append([np.asarry(blend_tiles(t, tiles)) for t in row])
+
+    rows = np.array(row)
+    n_rows, n_cols, tile_height, tile_width, _ = rows.shape
+    images = np.swapaxes(rows, 1, 2)
+    return Image.fromarray(images.reshape(n_rows * tile_height, n_cols * tile_width, 4))
+
+
